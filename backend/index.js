4261e616e0006b6d16ff281523685d8db32e7e4d
RomanCode <ozrv@inbox.ru>
2026-01-14 00:47:08 +0500

    Написал основу для бекенда, затем пришлось добавлять новые компоненты и подгонять старые

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'my_space_clone_app_db',
+    password: 'Veronica0404',
+    port: 5432
+});
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({ message: 'Server works!', time: result.rows[0] });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+ app.listen(5000, () => console.log('Server running on port 5000'));
+
+ app.get('/tasks', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM tasks ORDER BY id DESC');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+ });
+
