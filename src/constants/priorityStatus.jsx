0ae9f5508b3b848b1d9785c52ab4bfa37d7f01e9
RomanCode <ozrv@inbox.ru>
2026-04-04 21:12:49 +0500

    Добавил енумы для приоритетов задач

---
+++ src/constants/priorityStatus.jsx
@@ -0,0 +1,7 @@
+export const PRIORITY_STATUS = [
+    { value: 'stopper', label: 'Show-stopper', color: 'red'},
+    { value: 'crit', label: 'critical', color: 'pink'},
+    { value: 'major', label: 'major', color: 'yellow'},
+    { value: 'normal', label: 'normal', color: 'green'},
+    { value: 'minor', label: 'minor', color: 'gray'}
+]
